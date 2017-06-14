75809d2181e377e628cd8ef4859c2ad153eacbc9
Ethan Brox <brox.ethan@gmail.com>
2017-06-13 20:33:32 -0500

    tried
    tried to make a few changes, did not go well.

--- Project/app.js
+++ Project/app.js
@@ -12,7 +12,48 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000);
 console.log('server started');
 
+var player = {
+	id: null,
+	health: 100,
+	x: 50,
+	y: 50,
+	players: [],
+	
+	create: function(id){
+		var obj = Object.create(this);
+		this.id = id;
+		return obj;
+	}
+};
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
+	
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+	var p = player.create(socket.id);
+	PLAYER_LIST[p.id] = p;
+	
 	console.log('socket connection');
-})
+	
+	socket.on('disconnect', function(){
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[p.id];
+	})
+});
+
+setInterval(function(){
+	for(var i in PLAYER_LIST){
+		var p = PLAYER_LIST[i];
+		var players = [];
+		for(var i in PLAYER_LIST){
+			
+		}
+		socket.emit('players', {
+			id: socket.id
+		});
+	}
+}, 1000 / 25);
