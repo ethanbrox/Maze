d1da72de594205cfd0a71816c7d78d83a03751e5
Ethan Brox <brox.ethan@gmail.com>
2017-06-17 20:27:08 -0500

    Final Fix
    I went through and recreated the whole thing in a different file in the attempt to fix the problems I was having, I finally fixed them and completelly redid the files entirely, along with updating socet.io

---
+++ _Project/app.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+//If the query is anything not otherwise specified do this.
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/client/index.html');
+});
+//If the server specifies something specific but it has to be in the client folder.
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log('server started');
+
+var PLAYER_LIST = {};
+
+var Player = {
+	socket: null,
+	id: null,
+	health: 100,
+	x: 250,
+	y: 250,
+	roomId: null,
+	pressingRight: false,
+	pressingLeft: false,
+	pressingUp: false,
+	pressingDown: false,
+	speed: 10,
+	
+	create: function(){
+		var obj = Object.create(this);
+		return obj;
+	},
+	
+	updatePosition: function(){
+		if(this.pressingRight){
+			this.x += this.speed;
+		} 
+		if(this.pressingLeft){
+			this.x -= this.speed;
+		}
+		if(this.pressingUp){
+			this.y -= this.speed;
+		}
+		if(this.pressingDown){
+			this.y += this.speed;
+		}
+	},
+	
+};
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+	
+	socket.id = Math.random();
+	var player = Player.create();
+	player.id = socket.id;
+	player.socket = socket;
+	
+	var color = 'rgb('+
+		Math.floor(Math.random()*256)+','+
+      	Math.floor(Math.random()*256)+','+
+      	Math.floor(Math.random()*256)+')';
+	player.color = color;
+	
+	PLAYER_LIST[socket.id] = player;
+	console.log("player Created: " + player.id);
+	
+	socket.on('disconnect', function(){
+		delete PLAYER_LIST[socket.id];
+	});
+	
+	socket.on('keypress', function(data){
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		if(data.inputId === 'down')
+			player.pressingDown = data.state;
+	});
+	
+	socket.on('join', function(data){
+		console.log('join');
+		for(var i in PLAYER_LIST){
+			var player = PLAYER_LIST[i];
+			player.roomId = '1';
+		}
+	});
+});
+
+setInterval(function(){
+	for(var i in PLAYER_LIST){
+		var player = PLAYER_LIST[i];
+		player.updatePosition();
+		var pack = [];
+		pack.push({
+			x:player.x,
+			y:player.y,
+			color:player.color
+		});
+		
+		for(var k in PLAYER_LIST){
+			var p = PLAYER_LIST[k];
+			if(p.id != player.id){
+				if(p.roomId != null && player.roomId != null){
+					if(p.roomId == player.roomId){
+						pack.push({
+							x:p.x,
+							y:p.y,
+							color:p.color
+						});
+					}
+				}
+			}
+		}
+		
+		player.socket.emit('update', pack);
+	}
+}, 1000 / 25);
